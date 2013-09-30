5f48eb5012a0a85ea972ee3ec37afd93bbf26ad0
mattmr <mmr@iastate.edu>
2013-09-30 00:22:02 -0500

    skeleton code

---
+++ include/serial.h
@@ -0,0 +1,14 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+
+class serial
+{
+    public:
+        serial(send, listen, receive);
+        virtual ~serial();
+    protected:
+    private:
+};
+
+#endif // SERIAL_H
---
+++ src/serial.cpp
@@ -0,0 +1,26 @@
+#include "serial.h"
+
+serial::serial()
+{
+    //ctor
+}
+
+serial::send()
+{
+
+}
+
+serial::listen()
+{
+
+}
+
+serial::get()
+{
+
+}
+
+serial::~serial()
+{
+    //dtor
+}
